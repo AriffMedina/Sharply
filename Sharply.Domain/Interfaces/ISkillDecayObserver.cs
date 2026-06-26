e8add709e091c4d0eb230530b86d21ae9cc9b5ca
AriffMedina <ariffmedina.contact@gmail.com>
2026-06-26 15:54:57 -0600

    FEAT: agregar interfaces para implementar el patrón observer

---
+++ Sharply.Domain/Interfaces/ISkillDecayObserver.cs
@@ -0,0 +1,12 @@
+﻿using Sharply.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharply.Domain.Interfaces
+{
+    public interface ISkillDecayObserver
+    {
+        void Update(Skill skillAtRisk, User user);
+    }
+}
---
+++ Sharply.Domain/Interfaces/ISkillDecaySubject.cs
@@ -0,0 +1,14 @@
+﻿using Sharply.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharply.Domain.Interfaces
+{
+    public interface ISkillDecaySubject
+    {
+        void Attach(ISkillDecayObserver observer);
+        void Detach(ISkillDecayObserver observer);
+        void Notify(Skill skillAtRisk, User user);
+    }
+}
