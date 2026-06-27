e247cb1036c79c55a3be4eaf3f2a34e25d11e2ea
AriffMedina <ariffmedina.contact@gmail.com>
2026-06-26 18:13:28 -0600

    CHORE: crear migracion AddUserPasswordAndSetupDB y actualizar base de datos

--- Sharply.Infrastructure/Migrations/20260627000657_AddUserPasswordAndSetupDB.Designer.cs
+++ Sharply.Infrastructure/Migrations/20260627000657_AddUserPasswordAndSetupDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sharply.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Sharply.Infrastructure.Data;
 namespace Sharply.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260627000657_AddUserPasswordAndSetupDB")]
+    partial class AddUserPasswordAndSetupDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
