4ada21ceeb577beb255b8fdde9870684a50df290
AriffMedina <ariffmedina.contact@gmail.com>
2026-07-15 21:39:47 -0600

    refactor: reorganización de carpetas del proyecto

---
+++ Sharply.Infrastructure/Migrations/20260716014612_SyncModelChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sharply.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "InitialRetention",
+                table: "Skills",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitialRetention",
+                table: "Skills");
+        }
+    }
+}
